d4b85c85ccde4ad832f48dbd0a9b4b2b108e175f
Andreev Vladimir Yurievich <djdfy1103@gmail.com>
2024-03-10 09:09:21 +0500

    registrationSection

--- frontend/src/component/RegistrationSection.jsx
+++ frontend/src/component/RegistrationSection.jsx
@@ -1,48 +1,96 @@
-import FormGroup from "./forms";
-import Button from "./Button/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import React from "react";
+import axios from "axios";
+import { useState } from "react";
+import { Typography, Box, TextField } from "@mui/material";
+import Button from "@mui/material/Button";
+
+const url = "http://127.0.0.1:5000/add_applicant ";
 
 export default function Registration() {
+  const [id, setId] = useState(null);
+  const [name, setName] = useState("");
+  const [surName, setSurName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState(null);
+
+  const handleSubmit = () => {
+    axios.post(url, { id, name, surName, email, password });
+  };
+
+  // axios({
+  //   method: "post",
+  //   url: url,
+  //   data: {
+  //     id: Number,
+  //     name: name,
+  //     Surname: surName,
+  //     Email: email,
+  //     Password: password,
+  //   },
+  // });
+
   return (
-    <main>
-      <div className="register-form-container">
-      <h1 className="form-title">Регистрация</h1>
-      
-      <div className="form-fields">
-        <FormGroup 
-            type="text" 
-            id="User-Name" 
-            placeholder="Введите имя" 
-        />
-
-        <FormGroup
+    <>
+      <CssBaseline />
+      <Box
+        maxWidth="415px"
+        width="100%"
+        bgcolor="#ffffff"
+        boxShadow="0px 6px 50px rgba(217, 229, 225, 0.7)"
+        borderRadius="20px"
+        paddingLeft="30px"
+        paddingRight="30px"
+        paddingTop="38px"
+        paddingBottom="38px"
+      >
+        <Typography
+          variant="h1"
+          color="#30507d"
+          fontWeight="500"
+          fontSize="20px"
+          lineHeight="23px"
+          marginBottom="38px"
+        >
+          Регистрация
+        </Typography>
+        <TextField
+          autoFocus
+          margin="dense"
+          id="name"
+          label="Имя"
           type="text"
-          id="First-Name"
-          placeholder="Введите вашу фамилию"
+          fullWidth
         />
-
-        <FormGroup 
-        type="email" 
-        id="email" 
-        placeholder="Введите Email" 
+        <TextField
+          autoFocus
+          margin="dense"
+          id="firstName"
+          label="Фамилия"
+          type="text"
+          fullWidth
         />
-
-        <FormGroup 
-        type="password" 
-        id="password" 
-        placeholder="Введите Пароль" 
+        <TextField
+          autoFocus
+          margin="dense"
+          id="email"
+          label="Email Adress"
+          type="email"
+          fullWidth
         />
-
-        <FormGroup
+        <TextField
+          autoFocus
+          margin="dense"
+          id="pass"
+          label="Password"
           type="password"
-          id="password"
-          placeholder="Введите Пароль ещё раз"
+          fullWidth
         />
-      </div>
-      
-      <Button>Регистрация</Button>
-      
-    </div>
-    </main>
-    
+
+        <Button onClick={handleSubmit} variant="contained" fullWidth>
+          Регистрация
+        </Button>
+      </Box>
+    </>
   );
 }

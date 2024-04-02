4c44d4beec5d0ac6bfc56991cacaab24a8dc1c89
InnsmouthFisherman <76620415+InnsmouthFisherman@users.noreply.github.com>
2024-04-02 19:29:22 +0500

    fixes

--- assistant-backend/src/analytics/router.py
+++ assistant-backend/src/analytics/router.py
@@ -1,23 +1,19 @@
 from fastapi import APIRouter
-from .analytics import result, students_85_plus
+from .analytics import result
 
 router = APIRouter(
     prefix="/analytics",
     tags=["analytics"]
 )
 
-
-@router.get("/test")
-def get_analysis():
-    return result()
-
-
-@router.get("/highpoints")
-def get_high_points():
-    return students_85_plus()
-
-
+# сейчас у нас есть функции:
+# для результатов егэ students_n_score_plus()
+# формы подачи документов submission_of_documents()
+# возрастов get_ages()
+# городов и школ get_cities()
+# необходимо чтобы функция принимала нужные параметры (которые могут быть необязательными) и возвращала данные всех
+# функций, для которых указаны нужные параметры
 @router.post("/")
-def post_obrabotka(score, olympiads, direction, university):
+def analytics(score, olympiads, direction, university):
     pass
 

33e18391f48a7d21a1a35555794b307f73b0c3ca
KaidalovIlya <kaydalov.2026@mail.ru>
2024-04-18 21:55:23 +0500

    Fixes

--- assistant-backend/src/analytics/router.py
+++ assistant-backend/src/analytics/router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from .analytics import result, get_ages, get_cities, students_n_score_plus, submission_of_documents, data2_table, data_table, get_schools, mean_points_ege, direction_priority
+from .analytics import result, get_ages, get_cities, students_n_score_plus, submission_of_documents, data2_table, data_table, get_schools, mean_points_ege, direction_priority, portrait_of_student, convert_DataFrame
 
 router = APIRouter(
     prefix="/analytics",
@@ -7,16 +7,16 @@ router = APIRouter(
 )
 
 @router.post("/")
-def portrait_users(*args): pass
-
-
-
-
+def portrait_users():
+    students_portrait = portrait_of_student(dataframe)
+    return students_portrait
 
 @router.post("/filter")
 def filter(score, way):
     students_result = students_n_score_plus(data2_table, int(score))
     students_result2 = submission_of_documents(data2_table, way)
+    students_result = convert_DataFrame(students_result)
+    students_result2 = convert_DataFrame(students_result2)
     result = {"points": students_result, "way": students_result2}
     return result
 
@@ -27,5 +27,5 @@ def search():
     stud_school = get_schools(data_table)
     stud_ege = mean_points_ege(data2_table)
     stud_prior = direction_priority(data2_table)
-    result = {"age": stud_age, "city": stud_cities, "school": stud_school, "ege": stud_ege, "priotity": stud_prior}
-    return result
+    result = {"age": convert_DataFrame(stud_age), "city": convert_DataFrame(stud_cities), "school": convert_DataFrame(stud_school), "ege": convert_DataFrame(stud_ege), "priotity": convert_DataFrame(stud_prior)}
+    return convert_DataFrame(result)

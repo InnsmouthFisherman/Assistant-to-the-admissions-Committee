54c0b0c5f1bfd4204b4b72521de9632fa299422c
KomarovMaxim <masanjapositive@gmail.com>
2024-05-20 03:07:45 +0500

    Front-end-maximka

--- frontend/src/component/student-analysis/Selections/DataSearch.jsx
+++ frontend/src/component/student-analysis/Selections/DataSearch.jsx
@@ -9,6 +9,7 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
 import axios from "axios";
+import './styles.css'; // Import the CSS file
 
 const url = "http://127.0.0.1:5000/analytics/brief_analytics";
 
@@ -16,7 +17,9 @@ const DataSearch = () => {
   const [data, setData] = useState({
     ages: [],
     cities: [],
-    scores: [],
+    docEdu: [],
+    sumEge: [],
+    avgEge: [],
     schools: [],
     priorityDirections: [],
   });
@@ -30,18 +33,24 @@ const DataSearch = () => {
         const cityData = resp.data.city || {};
         const schoolData = resp.data.school || {};
         const prioritiesData = resp.data.priotity || {};
-        const scoresData = resp.data.ege["Сумма баллов"] || {};
+        const docEduData = resp.data.doc_edu || {};
+        const sumEgeData = resp.data.sum_ege || {};
+        const avgEgeData = resp.data.ege || {};
 
         const ages = Object.keys(ageData).map(age => `${age} (${ageData[age]})`);
         const cities = Object.keys(cityData).map(city => `${city} (${cityData[city]})`);
         const schools = Object.keys(schoolData).map(school => `${school} (${schoolData[school]})`);
         const priorities = Object.keys(prioritiesData).map(priority => `${priority} (${prioritiesData[priority]})`);
-        const scores = Object.keys(scoresData).map(score => `${score} (${scoresData[score]})`);
+        const docEdu = Object.keys(docEduData).map(doc => `${doc} (${docEduData[doc]})`);
+        const sumEge = Object.keys(sumEgeData).map(sum => `${sum} (${sumEgeData[sum]})`);
+        const avgEge = Object.keys(avgEgeData).map(avg => `${avg} (${avgEgeData[avg]})`);
 
         setData({
           ages,
           cities,
-          scores,
+          docEdu,
+          sumEge,
+          avgEge,
           schools,
           priorityDirections: priorities,
         });
@@ -56,51 +65,63 @@ const DataSearch = () => {
   const maxLength = Math.max(
     data.ages.length,
     data.cities.length,
-    data.scores.length,
+    data.docEdu.length,
+    data.sumEge.length,
+    data.avgEge.length,
     data.schools.length,
     data.priorityDirections.length
   );
 
   return (
-    <div>
+    <div className="background-container">
       <CssBaseline />
-      <Button
-        onClick={handleSearch}
-        variant="contained"
-        style={{ marginBottom: 38 }}
-      >
-        Поиск
-      </Button>
-
-      <div style={{ maxHeight: 400, overflowY: "auto" }}>
-        <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 1500, color: "#4471f3" }} aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell>ВОЗРАСТ (КОЛ-ВО)</TableCell>
-                <TableCell align="right">ГОРОДА (КОЛ-ВО)</TableCell>
-                <TableCell align="right">СУММА БАЛЛОВ (КОЛ-ВО)</TableCell>
-                <TableCell align="right">ШКОЛЫ (КОЛ-ВО)</TableCell>
-                <TableCell align="right">ПРИОРИТ-ЫЕ НАПРАВЛЕНИЯ (КОЛ-ВО)</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {Array.from({ length: maxLength }).map((_, index) => (
-                <TableRow key={index}>
-                  <TableCell>{data.ages[index] || ''}</TableCell>
-                  <TableCell align="right">{data.cities[index] || ''}</TableCell>
-                  <TableCell align="right">{data.scores[index] || ''}</TableCell>
-                  <TableCell align="right">{data.schools[index] || ''}</TableCell>
-                  <TableCell align="right">{data.priorityDirections[index] || ''}</TableCell>
+      <div className="triangle-left"></div>
+      <div className="triangle-right"></div>
+      <div className="content">
+        <div className="heading">Быстрая аналитика</div> {/* Вставляем заголовок */}
+        <div className="button-container">
+          <Button
+            onClick={handleSearch}
+            variant="contained"
+            style={{ marginBottom: 20 }}
+          >
+            Поиск
+          </Button>
+        </div>
+        
+        <div className="table-container">
+          <TableContainer component={Paper}>
+            <Table stickyHeader aria-label="sticky table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>ВОЗРАСТ (КОЛ-ВО)</TableCell>
+                  <TableCell align="right">ГОРОДА (КОЛ-ВО)</TableCell>
+                  <TableCell align="right">ДОК. ОБ ОБРАЗОВАНИИ (КОЛ-ВО)</TableCell>
+                  <TableCell align="right">СУММА БАЛЛОВ ЗА ЕГЭ (КОЛ-ВО)</TableCell>
+                  <TableCell align="right">СРЕДНИЙ БАЛЛ ЕГЭ (КОЛ-ВО)</TableCell>
+                  <TableCell align="right">ШКОЛЫ (КОЛ-ВО)</TableCell>
+                  <TableCell align="right">ПРИОРИТ-ЫЕ НАПРАВЛЕНИЯ (КОЛ-ВО)</TableCell>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+              </TableHead>
+              <TableBody>
+                {Array.from({ length: maxLength }).map((_, index) => (
+                  <TableRow key={index}>
+                    <TableCell>{data.ages[index] || ''}</TableCell>
+                    <TableCell align="right">{data.cities[index] || ''}</TableCell>
+                    <TableCell align="right">{data.docEdu[index] || ''}</TableCell>
+                    <TableCell align="right">{data.sumEge[index] || ''}</TableCell>
+                    <TableCell align="right">{data.avgEge[index] || ''}</TableCell>
+                    <TableCell align="right">{data.schools[index] || ''}</TableCell>
+                    <TableCell align="right">{data.priorityDirections[index] || ''}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
       </div>
     </div>
   );
 };
 
 export default DataSearch;
-  

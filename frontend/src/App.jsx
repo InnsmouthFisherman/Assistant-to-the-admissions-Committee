a82da62ae5cc501922a06aa0610cca0205142d0b
Andreev Vladimir Yurievich <djdfy1103@gmail.com>
2024-05-20 08:29:45 +0500

    2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,32 @@
-// App.js
-
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Registration from "./component/RegistrationSection";
 import SelectionFunction from "./component/student-analysis/SelectionFunction";
 import Application from "./component/Application";
 import PortraitApplican from "./component/student-analysis/Selections/PortraitApplicant";
-import { createTheme } from "@mui/material/styles";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import DataSearch from "./component/student-analysis/Selections/DataSearch";
 import Filter from "./component/student-analysis/Selections/filter/Filter";
 import InstallTably from "./component/student-analysis/installTably/InstallTablу";
 import FilterResult from "./component/student-analysis/Selections/filter/FilterResult";
-import Enter from "./component/student-analysis/Selections/EnterSection";
-
-import { createGlobalStyle } from 'styled-components';
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    background-color: #FBFAE7;
-    margin: 0; /* Установка отступов по умолчанию для body */
-    padding: 0; /* Установка отступов по умолчанию для body */
-  }
-`;
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 export default function App() {
   return (
-    <>
-      <GlobalStyle />
-      <Routes>
-        <Route path="/" element={<Registration />} />
-        <Route path="/Enter/:type" element={<Enter />} />
-        <Route path="/install/:type" element={<InstallTably />} />
-        <Route path="/selectFunc" element={<SelectionFunction />} />
-        <Route path="/portrait" element={<PortraitApplican />} />
-        <Route path="/filter" element={<Filter />} />
-        <Route path="/dataSearch" element={<DataSearch />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<InstallTably />} />
+      <Route path="/selectFunc" element={<SelectionFunction />} />
+      <Route path="/portrait" element={<PortraitApplican />} />
+      <Route path="/filter" element={<Filter />} />
+      <Route path="/dataSearch" element={<DataSearch />} />
+      <Route path="/filterResult" element={<FilterResult />} />
+    </Routes>
+    // <DataSearch theme={darkTheme}>
+    //   <CssBaseline />
+    //   <Analysis />
   );
 }
